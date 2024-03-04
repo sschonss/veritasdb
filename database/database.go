c687008cf4bb805d7aa565939566824020211775
sschonss <schonsluuiz@gmail.com>
2024-03-04 19:28:21 -0300

    Adiciona configuração de usuário e senha e remove tabela não utilizada

---
+++ database/database.go
@@ -0,0 +1,123 @@
+package database
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Database struct {
+	User     string
+	Password string
+}
+
+func (Database) ExecuteQuery(query string) string {
+	command := strings.Fields(query)[0]
+	switch strings.ToLower(command) {
+	case "clear":
+		fmt.Print("\033[H\033[2J")
+	case "select":
+		fmt.Println("Executando comando SELECT")
+	case "insert":
+		fmt.Println("Executando comando INSERT")
+	case "update":
+		fmt.Println("Executando comando UPDATE")
+	case "delete":
+		fmt.Println("Executando comando DELETE")
+	case "create":
+		fmt.Println("Executando comando CREATE")
+	case "drop":
+		fmt.Println("Executando comando DROP")
+	case "alter":
+		fmt.Println("Executando comando ALTER")
+	default:
+		fmt.Println("Comando não reconhecido")
+	}
+
+	return "Resultado da execução do comando: " + query
+}
+
+func (Database) SaveConfig(user, password string) {
+	fmt.Println("Salvando configuração...")
+
+	configFile, err := os.Create("data/config.txt")
+	if err != nil {
+		fmt.Println("Erro ao criar arquivo de configuração")
+		return
+	}
+	defer configFile.Close()
+
+	_, err = configFile.WriteString("user:" + user + "\n")
+	if err != nil {
+		fmt.Println("Erro ao salvar usuário no arquivo de configuração")
+		return
+	}
+
+	_, err = configFile.WriteString("password:" + password + "\n")
+	if err != nil {
+		fmt.Println("Erro ao salvar senha no arquivo de configuração")
+		return
+	}
+
+	fmt.Println("Configuração salva com sucesso!")
+
+}
+
+func (Database) LoadConfig() (string, string) {
+	fmt.Println("Carregando configuração...")
+
+	configFile, err := os.Open("data/config.txt")
+	if err != nil {
+		fmt.Println("Erro ao abrir arquivo de configuração")
+		return "", ""
+	}
+	defer configFile.Close()
+
+	var user, password string
+	fmt.Fscanf(configFile, "user:%s\n", &user)
+	fmt.Fscanf(configFile, "password:%s\n", &password)
+
+	fmt.Println("Configuração carregada com sucesso!")
+
+	return user, password
+}
+
+func (Database) CheckConfig() bool {
+	_, err := os.Stat("data/config.txt")
+	if os.IsNotExist(err) {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (Database) CheckUser(user, password string) bool {
+	savedUser, savedPassword := Database{}.LoadConfig()
+	return user == savedUser && password == savedPassword
+}
+
+func (Database) CreateUser(user, password string) {
+	Database{}.SaveConfig(user, password)
+}
+
+func (Database) UpdateUser(user, password string) {
+	Database{}.SaveConfig(user, password)
+}
+
+func (Database) DeleteUser() {
+	os.Remove("data/config.txt")
+}
+
+func (Database) GetUsers() []string {
+	return []string{"admin"}
+}
+
+func GetUserInput() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+
+
--- main.go
+++ main.go
@@ -1,105 +1,62 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 
-	"veritasdb/table"
+	"veritasdb/database"
 )
 
 func main() {
 
-	configFile, err := os.Open("data/login.txt")
-	var tb *table.Table
-	if err != nil {
-		fmt.Println("Arquivo de configuração não encontrado. Por favor, insira as informações de login.")
+	var db database.Database
+
+	config := db.CheckConfig()
+	if config == true {
+		fmt.Println("Configurações encontradas.")
+		fmt.Println("Digite o usuário e a senha para carregar as configurações.")
 		fmt.Print("Usuário: ")
-		user := getUserInput()
+		user := database.GetUserInput()
 		fmt.Print("Senha: ")
-		password := getUserInput()
-		
-		saveConfig(user, password)
-
-
-	} else {
-		scanner := bufio.NewScanner(configFile)
-		scanner.Scan()
-		user := strings.Split(scanner.Text(), ":")[1]
-		scanner.Scan()
-		password := strings.Split(scanner.Text(), ":")[1]
-
-		fmt.Print("Usuário: ")
-		inputUser := getUserInput()
-		fmt.Print("Senha: ")
-		inputPassword := getUserInput()
-
-		for i := 0; i < 3; i++ {
-			if inputUser == user && inputPassword == password {
-				break
-			} else {
-				fmt.Println("Usuário ou senha inválidos. Tente novamente.")
-				fmt.Print("Usuário: ")
-				inputUser = getUserInput()
-				fmt.Print("Senha: ")
-				inputPassword = getUserInput()
+		password := database.GetUserInput()
+		valid := db.CheckUser(user, password)
+		count_try := 0
+		for valid == false {
+			count_try++
+			if count_try > 2 {
+				fmt.Println("Número de tentativas excedido.")
+				return
 			}
+			fmt.Println("Usuário ou senha inválidos.")
+			fmt.Print("Usuário: ")
+			user = database.GetUserInput()
+			fmt.Print("Senha: ")
+			password = database.GetUserInput()
+			valid = db.CheckUser(user, password)
 		}
-		if inputUser != user || inputPassword != password {
-			fmt.Println("Usuário ou senha inválidos. Tente novamente.")
-			return
-		}
-			
+		db = database.Database{User: user, Password: password}
+
+	}else {
+		fmt.Println("Nenhuma configuração encontrada.")
+		fmt.Println("Digite o usuário e a senha para salvar as configurações.")
+		fmt.Print("Usuário: ")
+		user := database.GetUserInput()
+		fmt.Print("Senha: ")
+		password := database.GetUserInput()
+		db = database.Database{User: user, Password: password}
+		db.CreateUser(user, password)
 	}
+
+
 
 	fmt.Println("Bem-vindo ao VeritasDB!")
 
 	for {
-		fmt.Print("veritastb> ")
-		query := getUserInput()
+		fmt.Print("veritasdb> ")
+		query := database.GetUserInput()
 		if query == "exit" {
 			break
 		}
-		fmt.Println(tb.ExecuteQuery(query))
+		fmt.Println(db.ExecuteQuery(query))
 
 	}
 }
-
-func getUserInput() string {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	return scanner.Text()
-}
-
-func saveConfig(user, password string) {
-	if _, err := os.Stat("data"); os.IsNotExist(err) {
-		err := os.Mkdir("data", 0755)
-		if err != nil {
-			fmt.Println("Erro ao criar o diretório 'data':", err)
-			return
-		}
-	}
-
-	configFile, err := os.Create("data/login.txt")
-	if err != nil {
-		fmt.Println("Erro ao criar arquivo de configuração:", err)
-		return
-	}
-	defer configFile.Close()
-
-	_, err = configFile.WriteString("user:" + user + "\n")
-	if err != nil {
-		fmt.Println("Erro ao escrever no arquivo de configuração:", err)
-		return
-	}
-
-	_, err = configFile.WriteString("password:" + password + "\n")
-	if err != nil {
-		fmt.Println("Erro ao escrever no arquivo de configuração:", err)
-		return
-	}
-
-	fmt.Println("Configurações salvas com sucesso.")
-}
-
