01a3e19844783cbf14c28acb79f8b009f135ee22
sschonss <schonsluuiz@gmail.com>
2024-03-05 20:00:43 -0300

    Adicionado funcionalidade de seleção de colunas e condições na consulta SQL

--- commands/commands.go
+++ commands/commands.go
@@ -153,6 +153,78 @@ func SelectWhere(command string) {
 		SelectAllFromWhere(command)
 		return
 	}
+
+	arrayColumns := strings.Split(command, " ")[1]
+	arrayColumns = strings.Split(arrayColumns, "from")[0]
+	arrayColumns = strings.Replace(arrayColumns, " ", "", -1)
+	arrayColumns = strings.Split(arrayColumns, ",")[0]
+
+	fmt.Println("Colunas: " + arrayColumns)
+
+	table := strings.Split(command, " ")[3]
+
+	if _, err := os.Stat("data/" + table + ".csv"); os.IsNotExist(err) {
+		fmt.Println("Tabela não existe")
+		return
+	}
+
+	file, err := os.Open("data/" + table + ".csv")
+	if err != nil {
+		fmt.Println("Erro ao abrir arquivo")
+		return
+	}
+
+	defer file.Close()
+
+	fmt.Println("Tabela: " + table)
+
+	var columnsTable string
+	fmt.Fscanf(file, "%s\n", &columnsTable)
+	columnsTableArray := strings.Split(columnsTable, ";")
+
+	for i := 0; i < len(columnsTableArray); i++ {
+		if columnsTableArray[i] == arrayColumns {
+			fmt.Println(columnsTableArray[i])
+		}
+	}
+
+	operadores := []string{"=", ">", "<", ">=", "<=", "<>"}
+	conditions := strings.Split(command, "where")[1]
+	conditions = strings.Replace(conditions, " ", "", -1)
+	conditions = strings.Replace(conditions, "(", "", -1)
+	conditions = strings.Replace(conditions, ")", "", -1)
+	conditions = strings.Replace(conditions, ";", "", -1)
+	conditions_array := strings.Split(conditions, "and")
+
+	var operador string
+
+	for i := 0; i < len(operadores); i++ {
+		if strings.Contains(conditions_array[0], operadores[i]) {
+			operador = operadores[i]
+		}
+	}
+
+	valor := strings.Split(conditions_array[0], operador)[1]
+	valor = strings.Replace(valor, " ", "", -1)
+
+	var line string
+	for {
+		_, err := fmt.Fscanf(file, "%s\n", &line)
+		if err != nil {
+			break
+		}
+
+		if operador == "=" {
+			if strings.Contains(line, valor) {
+				lineArray := strings.Split(line, ";")
+				for i := 0; i < len(lineArray); i++ {
+					if columnsTableArray[i] == arrayColumns {
+						fmt.Println(lineArray[i])
+					}
+				}
+			}
+		}
+	}
 }
 
 func SelectAllFromWhere(command string)  {
